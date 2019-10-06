2264ca8e8ccd355919662f607fe5e32a3384875f
Lisicki Artur <artur.lisicki@microsoft.wsei.edu.pl>
2019-10-06 15:19:16 +0200

    zadanie 2

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,19 @@
+const distFromAvarage = (arr) => {
+    let avg = 0;
+    let sum = 0;
+    arr.forEach(item => {
+        sum += item;
+    })
+
+    avg = sum / arr.length;
+    let newArr = [];
+
+    arr.forEach(item => {
+        newArr.push(item - avg);
+    })
+
+    console.log(newArr);
+    return newArr;
+}
+
+distFromAvarage([1,2,3,4,5,6,7])
