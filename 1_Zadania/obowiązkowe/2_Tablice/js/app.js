9f98d8743e0c39c4c2a2288d34efe9fed9af756f
Lisicki Artur <artur.lisicki@microsoft.wsei.edu.pl>
2019-10-06 15:37:15 +0200

    zadanie 4

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,4 @@
+// zadanie 0
 const distFromAvarage = (arr) => {
     let avg = 0;
     let sum = 0;
@@ -17,3 +18,34 @@ const distFromAvarage = (arr) => {
 }
 
 distFromAvarage([1,2,3,4,5,6,7])
+
+// zadanie 1
+const fruits = ["apple", "orange", "banana", "pineapple"];
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+
+fruits.forEach(fruit => {
+    console.log(fruit); 
+});
+
+// zadanie 3
+function printTable(array) {
+    for(i=0; i<array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+printTable([1,2,3,4]);
+
+// zadanie 4
+function multiply(array) {
+    let sum = 1;
+    for(var i = 0; i < array.length; i++) {
+        sum *= array[i];
+    }    
+    console.log(sum);
+    return sum;
+}
+
+multiply([1,2,3,4,5,6,7]);
